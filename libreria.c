aee9f35def8bfeb1591499127cf85d5023dd0da7
jonmazh <jonmazh@gmail.com>
2024-10-19 10:09:24 +0200

    Gestión de errores y memoria dinámica
    (Versión sin compilación comprobada)

--- libreria.c
+++ libreria.c
@@ -3,45 +3,55 @@
 #include <errno.h> 
 #include <string.h> 
 #include <stdlib.h> 
+#include <unistd.h> 
 
 
 
 
 int main(int argc, char *argv[])
 {
-  //TODO
+  char *MSGERR = "Use ./libreria.c head [DIRECTORIO].";
+  head(argc); 
   return 0; 
 }
 
 // Función que muestra las N primeras líneas de la entrada estándar.
+// Funciones E/es = fscanf
+// @param buffer Memoria estática para guardar la entrada estándar
+// @param header Memoria dinámica donde almaceno la cabecera
 // Devueve 0 en caso de éxito y -1 en caso de error.
-int head(char buf[1024]) 
+int head(int argc) 
 {
+	char buffer[1024];
+	char *header; 
+	size_t size = 100; 
 	char *MSGERR = "Use ./libreria.c head [DIRECTORIO].";
-	if (argc==1){
-           printf("%s", MSGERR);
-	  return -1 ; 
+
+	header = (char *)malloc(size * sizeof (char)); 
+	if (header == NULL){
+		printf("%s",MSGERR); 
+		return -1; 
 	}
-	else{
-		if (argc>2)
-		{
-		  printf("%s", MSGERR); 
-		  return -1 ;
+	
+	if (argc==1) {
+        printf("%s", MSGERR);
+	  	return -1 ; 
+	} else if(argc>2) {
+		printf("%s", MSGERR); 
+		return -1 ;
+	} else {
+		if ((fscanf(stdin, "%s", buffer))>0){
+			header = strdup(buffer);
+			printf("%s", header);
+			free(header);
+			return 0; 
 		}
 		else{
-		     if(!access(buf, X_OK) 
-		     {
-		     //TODO FUNCIONALIDAD
-		     	return 0 ; 
-		     }
-                     else
-		     {
-		      	return -1 ; 
-		     }
+			return -1; 
 		}
 	}
-
-	return 0; 
+	//EOF HEAD
 }
+
 
 
